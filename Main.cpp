4483d7a55013130efd51d992535fef5820040f66
Cooper Sheets <coopersheets@gmail.com>
2019-10-06 10:37:46 -0500

    Made the gridValues array a private member of main(). Adjusted DrawBoard to take values in as a paramteter.

--- Main.cpp
+++ Main.cpp
@@ -1,11 +1,9 @@
 #include <iostream>
 #include <string>
+
 using namespace std;
 
-// Create the values for the grids on the board
-string gridValues[9] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
-
-void DrawBoard()
+void DrawBoard(const string values[])
 {
 	// Clear the Screen
 	system("CLS");
@@ -23,16 +21,19 @@ void DrawBoard()
 
 	*/
 
-	cout << gridValues[0] << " | " << gridValues[1] << " | " << gridValues[2] << endl;
+	cout << values[0] << " | " << values[1] << " | " << values[2] << endl;
 	cout << "---------" << endl;
-	cout << gridValues[3] << " | " << gridValues[4] << " | " << gridValues[5] << endl;
+	cout << values[3] << " | " << values[4] << " | " << values[5] << endl;
 	cout << "---------" << endl;
-	cout << gridValues[6] << " | " << gridValues[7] << " | " << gridValues[8] << endl;
+	cout << values[6] << " | " << values[7] << " | " << values[8] << endl;
 }
 
 int main()
 {
-	DrawBoard();
+	// Create the values for the grids on the board
+	string gridValues[9] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+
+	DrawBoard(gridValues);
 
 	return 0;
 }
